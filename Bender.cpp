9f15a2583158d170197f172d2a82ebe4526e3188
Dmendoza3 <Dmendoza3@unitec.edu>
2017-03-17 17:04:26 -0600

    Revision final final

--- Bender.cpp
+++ Bender.cpp
@@ -9,32 +9,3 @@ Bender::Bender(string nnombre, double nhp, double ndefensa, double nofensa, doub
 	fuerza = nfuerza;
 	suerte = nsuerte;
 }
-
-/*double Bender::roll()
-{
-	if(suerte <= 5)
-	{
-		if(rand() % 1000 == 50)
-			return 0.50;
-	}
-	else if(suerte <= 9)
-	{	
-		if((rand() % 100) % 5 == 0)
-			return 0.50;
-	}
-	else
-	{
-		if((rand() % 100) % 3 == 0)
-			return 0.50;
-	}
-	return 0;
-}
-
-double Bender::ataque(Bender* enemy)
-{
-    double nhp = enemy->hp;
-    nhp = (ofensa * (1 + roll())) - enemy->defensa;
-    enemy->hp -= ((nhp < 0)?0:nhp);
-
-	return nhp;
-}*/
